9e8ce296c2918c11ce1954dcad4f0933637a99a8
Rru@no44 <rruanoh@miumg.edu.gt>
2021-05-10 12:04:27 -0600

    nuevo inicio rene

--- view/V_usu_externos.php
+++ view/V_usu_externos.php
@@ -15,28 +15,32 @@
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e9454c;color:white;" >
       <div class="container-fluid" style="background-color: #e9454c;color:white;">
 
-      <?php
-        session_start();
-       echo '<a class="navbar-brand" href="#" style="background-color: #e9454c;color:white;">' . $_SESSION["usuario"] . '</a>';
-       ?> 
+    
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="background-color:white;color:white;" >
           <span class="navbar-toggler-icon" style="background-color:white;color:white;"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent" style="background-color: #e9454c;color:white;">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="background-color: #e9454c;color:white;">
-            
+          <br>
             <li class="nav-item" style="background-color: #e9454c;color:white;">
-              <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample" style="margin-left:5px;" >
-                expedientes
+            
+              <a class="btn btn-primary btn-s" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample" style="margin-left:5px;width:160px;margin-top:1px;" >
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-spreadsheet" viewBox="0 0 16 16">
+  <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V9H3V2a1 1 0 0 1 1-1h5.5v2zM3 12v-2h2v2H3zm0 1h2v2H4a1 1 0 0 1-1-1v-1zm3 2v-2h3v2H6zm4 0v-2h3v1a1 1 0 0 1-1 1h-2zm3-3h-3v-2h3v2zm-7 0v-2h3v2H6z"/>
+</svg> expedientes
                </a>
        
                <li class="nav-item" style="background-color: #e9454c;color:white;">
-              <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample1" role="button" aria-controls="offcanvasExample" style="margin-left:5px;">
-                Solicitudes
+              <a class="btn btn-primary btn-s" data-bs-toggle="offcanvas" href="#offcanvasExample1" role="button" aria-controls="offcanvasExample" style="margin-left:5px;width:160px;margin-top:1px;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-easel" viewBox="0 0 16 16">
+  <path d="M8.5 6a.5.5 0 1 0-1 0h-2A1.5 1.5 0 0 0 4 7.5v2A1.5 1.5 0 0 0 5.5 11h.473l-.447 1.342a.5.5 0 1 0 .948.316L7.027 11H7.5v1a.5.5 0 0 0 1 0v-1h.473l.553 1.658a.5.5 0 1 0 .948-.316L10.027 11h.473A1.5 1.5 0 0 0 12 9.5v-2A1.5 1.5 0 0 0 10.5 6h-2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-5a.5.5 0 0 1-.5-.5v-2z"/>
+  <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
+</svg> Solicitudes
                </a>
             </li>
             <li class="nav-item" style="background-color: #e9454c;color:white;">
-              <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample2" role="button" aria-controls="offcanvasExample" style="margin-left:5px;" >
+              <a class="btn btn-primary btn-s" data-bs-toggle="offcanvas" href="#offcanvasExample2" role="button" aria-controls="offcanvasExample" style="margin-left:5px;width:160px;margin-top:1px;" >
+              
                 Soporte Contacto
                </a>
 
@@ -48,6 +52,14 @@
           <form class="d-flex" style="background-color: #e9454c;color:white;">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" >
             <button class="btn btn-outline-success" type="submit" style="background-color: #009fe3;color:white;">Search</button>
+            <?php
+        session_start();
+        echo '';
+       echo '<a class="navbar-brand" href="#" style="background-color: #e9454c;color:white;margin-left:5%;text-align:center;font-size:8pt;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-badge" viewBox="0 0 16 16">
+       <path d="M6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+       <path d="M4.5 0A2.5 2.5 0 0 0 2 2.5V14a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2.5A2.5 2.5 0 0 0 11.5 0h-7zM3 2.5A1.5 1.5 0 0 1 4.5 1h7A1.5 1.5 0 0 1 13 2.5v10.795a4.2 4.2 0 0 0-.776-.492C11.392 12.387 10.063 12 8 12s-3.392.387-4.224.803a4.2 4.2 0 0 0-.776.492V2.5z"/>
+     </svg><br>' . $_SESSION["usuario"] . '</a>';
+       ?> 
           </form>
         </div>
       </div>
@@ -349,13 +361,13 @@
               <input type="email" class="form-control"  placeholder="Correo"required name="cor">
             </div>
             
-            <div class="mb-3">
+            <div class="mb-3" style="display:none;">
               <label for="exampleFormControlInput1" class="form-label">Codigo usuario</label>
               <?php             
               echo '<input type="text" class="form-control"  placeholder="Codigo"required name="cod" value="'. $_SESSION["id_usuario"]  . '">';
             ?>
               </div>
-              <input type="submit" value="registra"><!--esto sirve para para aplicar required-->
+              <input type="submit" value="Crear Solicitud"><!--esto sirve para para aplicar required-->
 
       </form>
            <!--fin de formulario--> 
